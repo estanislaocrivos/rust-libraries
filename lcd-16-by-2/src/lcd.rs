6308208a1edef6c305f43e9ffcc48d30b3630ae1
estanislaocrivos <estanislaocrivos@gmail.com>
2025-09-20 13:18:27 -0300

    Updates.

--- lcd-16-by-2/src/lcd.rs
+++ lcd-16-by-2/src/lcd.rs
@@ -1,5 +1,5 @@
 use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::{OutputPin, PinState};
 
 pub struct Lcd<RS, EN, D4, D5, D6, D7, DELAY>
 where
@@ -42,9 +42,57 @@ where
         }
     }
 
-    pub fn init(&mut self) {
-        self.delay.delay_ms(9);
+    fn assert_command_sequence(&mut self) {
         self.rs.set_low().unwrap();
-        // ...
+        self.en.set_high().unwrap();
+        self.delay.delay_ms(10);
+        self.en.set_low().unwrap();
     }
+
+    fn assert_data_sequence(&mut self) {
+        self.rs.set_high().unwrap();
+        self.en.set_high().unwrap();
+        self.delay.delay_ms(10);
+        self.en.set_low().unwrap();
+    }
+
+    fn pin_state(state: bool) -> PinState {
+        if state {
+            return PinState::High;
+        } else {
+            return PinState::Low;
+        }
+    }
+
+    fn write_nibble(&mut self, nibble: u8) {
+        self.d4
+            .set_state(Self::pin_state(((nibble >> 0) & 0x01) != 0))
+            .unwrap();
+        self.d5
+            .set_state(Self::pin_state(((nibble >> 1) & 0x01) != 0))
+            .unwrap();
+        self.d6
+            .set_state(Self::pin_state(((nibble >> 2) & 0x01) != 0))
+            .unwrap();
+        self.d7
+            .set_state(Self::pin_state(((nibble >> 3) & 0x01) != 0))
+            .unwrap();
+        self.assert_command_sequence();
+    }
+
+    fn write_command(&mut self, command: u8) {
+        self.write_nibble((command >> 4) & 0x0F);
+        self.assert_command_sequence();
+        self.write_nibble(command & 0x0F);
+        self.assert_command_sequence();
+    }
+
+    fn write_char(&mut self, char: u8) {
+        self.write_nibble((char >> 4) & 0x0F);
+        self.assert_data_sequence();
+        self.write_nibble(char & 0x0F);
+        self.assert_data_sequence();
+    }
+
+    pub fn init(&mut self) {}
 }
